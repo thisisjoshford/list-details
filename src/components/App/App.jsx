2c70d12630249a291a2344b6b50763b901228bdd
Josh Ford <thisisjoshford@gmail.com>
2020-04-29 14:32:59 -0700

    character list / details

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+import CharacterList from '../../containers/CharacterList';
+import CharacterDetails from '../../containers/CharacterDetails';
 import './App.css';
 
 export default function App() {
-  return <img id="logo" src="main.png"/>;
+  return ( 
+    <>
+      <header>
+        <img id="logo" src="main.png"/>
+      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/details/:charName" component={CharacterDetails}/>
+          <Route exact path="/" component={CharacterList} />
+        </Switch>
+      </Router>
+    </>
+  );
 } 
